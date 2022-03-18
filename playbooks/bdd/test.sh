260865bf47b63f1597691e79df95b02d4d3a5294
plubzzzz <bastien.grosclaude@orange.fr>
2022-03-18 15:52:00 +0100

    Update test.sh

--- playbooks/bdd/test.sh
+++ playbooks/bdd/test.sh
@@ -1,1 +1,5 @@
+echo "DELETE FROM mysql.user WHERE User='';" | mysql
+echo "CREATE USER 'test'@'localhost' IDENTIFIED BY 'password';"
+echo "GRANT ALL PRIVILEGES ON * . * TO `test`@`localhost`;"
+echo "FLUSH PRIVILEGES;"
 sudo mysql < /tmp/requete.sql
