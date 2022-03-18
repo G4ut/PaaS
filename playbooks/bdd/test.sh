1ece42199d593a2426a275d5bb00954767c746a3
plubzzzz <bastien.grosclaude@orange.fr>
2022-03-18 15:59:11 +0100

    Update and rename test.sh to template.j2

--- playbooks/bdd/test.sh
+++
@@ -1,5 +0,0 @@
-echo "DELETE FROM mysql.user WHERE User='';" | mysql
-echo "CREATE USER 'test'@'localhost' IDENTIFIED BY 'password';"
-echo "GRANT ALL PRIVILEGES ON * . * TO `test`@`localhost`;"
-echo "FLUSH PRIVILEGES;"
-sudo mysql < /tmp/requete.sql
