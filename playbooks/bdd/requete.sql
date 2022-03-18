a1cde343244858682575eae8b2286c043d35264c
plubzzzz <bastien.grosclaude@orange.fr>
2022-03-18 15:45:54 +0100

    Update requete.sql

--- playbooks/bdd/requete.sql
+++ playbooks/bdd/requete.sql
@@ -1,3 +1,3 @@
-CREATE USER 'aaaabaa'@'localhost' IDENTIFIED BY 'password';
+CREATE USER 'mabite'@'localhost' IDENTIFIED BY 'password';
 GRANT ALL PRIVILEGES ON * . * TO `user`@`localhost`;
 FLUSH PRIVILEGES;
