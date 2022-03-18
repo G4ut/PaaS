549aed1798561f295a679a0cab8b072239478c31
plubzzzz <bastien.grosclaude@orange.fr>
2022-03-18 15:39:20 +0100

    Update requete.sql

--- playbooks/bdd/requete.sql
+++ playbooks/bdd/requete.sql
@@ -1,4 +1,4 @@
-DELETE FROM mysql.user WHERE User=``;
-CREATE USER `aaaabaa`@`localhost` IDENTIFIED BY `password`;
+DELETE FROM mysql.user WHERE User='';
+CREATE USER 'aaaabaa'@'localhost' IDENTIFIED BY 'password';
 GRANT ALL PRIVILEGES ON * . * TO `user`@`localhost`;
 FLUSH PRIVILEGES;
